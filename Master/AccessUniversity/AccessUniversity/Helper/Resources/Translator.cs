f88f9c631275d3179657e4f92f04b1f1c247e921
Fidel Seng <Karuido@hotmail.com>
2020-10-11 22:18:16 +1000

    Localisation testing

---
+++ Master/AccessUniversity/AccessUniversity/Helper/Resources/Translator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AccessUniversity.Helper.Resources
+{
+    [ContentProperty("Text")]
+    class Translator : IMarkupExtension
+    {
+        const string ResourceId = "AccessUniversity.Helpers.Resources.AppResources";
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+            {
+                return null;
+            }
+                ResourceManager resman = new ResourceManager(ResourceId, typeof(Translator).GetTypeInfo().Assembly);
+                return resman.GetString(Text, CultureInfo.CurrentCulture);
+        }
+    }
+}
